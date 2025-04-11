9cc1e79fff78ca5b0ba401b31aaf36498623740e
Braveheart570 <42154318+Braveheart570@users.noreply.github.com>
2025-04-10 20:54:33 -0400

    Added special case for Brave Advance level.
    having the cleric cast auto life on himself creates an immoral punching bag. no matter what these fighters will always target an unprotected cleric. easy win.

--- Program.cs
+++ Program.cs
@@ -78,6 +78,26 @@ namespace PlayerCoder
                 if (AttemptOneShot()) return;
 
 
+
+                //brave advance logic
+                int fighterCount = 0;
+                foreach (Hero h in TeamHeroCoder.BattleState.foeHeroes)
+                {
+                    if (h.jobClass == HeroJobClass.Fighter)
+                    {
+                        fighterCount++;
+                    }
+                }
+                if(fighterCount == 3)
+                {
+                    Console.WriteLine("Brave Advance Detected, casting autolife");
+                    if(BuffNotBuffed(StatusEffect.AutoLife,Ability.AutoLife,activeHero))return;
+                }
+
+
+
+
+                // emergency heal
                 if (activeHero.health < activeHero.maxHealth*0.4f)
                 {
                     if (AttemptCastSpell(Ability.CureSerious, activeHero)) return;
